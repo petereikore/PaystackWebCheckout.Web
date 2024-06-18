e6e4515188a8b02ce2852ab72bbdbf43f9c82b27
peikore@vatebra.com <peikore@vatebra.com>
2024-06-18 08:19:57 +0100

    Ok

---
+++ FlutterwaveCheckout.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlutterwaveCheckout.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
